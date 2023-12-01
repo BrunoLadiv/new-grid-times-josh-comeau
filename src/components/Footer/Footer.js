da2d74986be140a631a4fcdaae083ef58c81f071
Bruno Vidal <brunokasprik@gmail.com>
2023-12-01 18:54:46 -0300

    exercise 5: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import { QUERIES } from '../../constants';
 
 const Footer = () => {
   return (
@@ -144,6 +145,14 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+  @media ${QUERIES.tabletAndUp}{
+    flex-direction: row;
+    justify-content: center;
+    gap: 50px;
+  }
+  @media ${QUERIES.laptopAndUp}{
+    justify-content: flex-end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +179,15 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp}{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    text-align: start;
+  }
+  @media ${QUERIES.laptopAndUp}{
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +214,9 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media ${QUERIES.laptopAndUp}{
+    align-items: baseline;
+  }
 `;
 
 const Logo = styled.a`
